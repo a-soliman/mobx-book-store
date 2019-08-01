ebe8b8600e5dabb54b01fefd05e2565667b4b279
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-08-01 08:03:37 -0700

    connected header component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
 import React, { useState } from 'react';
 import { inject, observer } from 'mobx-react';
+import Header from './components/Header';
+import styles from './App.css';
 
 
 const App = inject('Store')(observer(({ Store }) => {
-  const { items, itemsCount } = Store;
-
-  const [inputText, setInputText ] = useState('');
-
-  function handleChange(evt) {
-    setInputText(evt.target.value);
-  }
-
-  function handleSubmit(evt) {
-    evt.preventDefault();
-    if (inputText.length) {
-      Store.addItem(inputText);
-      setInputText('');
-    }
-  };
-
-  function handleCancel() {
-    setInputText('');
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-           <h2>React Mobx Boilerplate</h2>
-           <hr/>
-           <form onSubmit={handleSubmit}>
-             <label htmlFor="Add Item">
-               <input type="text" value={inputText} onChange={handleChange} />
-             </label>
-             <input type="submit" value='Add Item' disabled={!inputText} />
-            <input type="button" value='Cancel'  onClick={handleCancel} />
-           </form>
-           <ul>
-            {items.map((item, i) => (<li key={i}>{item}</li>))}
-           </ul>
-           <p>a total of {itemsCount}</p>
-         </header>
-       </div>
+    <main>
+      <Header />
+    </main>
   );
 }));
 
